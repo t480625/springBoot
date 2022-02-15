b2ee5eb99b13fbcf9b10225a94188b54bc1fe6f7
anna8fm88@gmail.com <A1@m3$ev>
2022-02-15 22:03:40 +0100

    merge master into feature-2

--- src/main/java/com/example/demo/aspect/LogAspect.java
+++ src/main/java/com/example/demo/aspect/LogAspect.java
@@ -18,7 +18,7 @@ public class LogAspect {
         System.out.println("Invocation of method" + jp.getSignature() );
     }
 
-    //comitte to
+    //comitte tov
     public void feature_2() {
         System.out.println();
     }
